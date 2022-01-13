809257db2c10eb2a95dd3aae4b8b1b78675b4e4f
Nadav Nassi <nadav.nassi@gmail.com>
2022-01-13 13:17:01 +0200

    thank modal fix

--- src/cmps/thank-modal.jsx
+++ src/cmps/thank-modal.jsx
@@ -3,10 +3,10 @@
 export const ThankModal = ({ setModal }) => {
     return (
         <>
-            <div className="screen" onClick={setModal(false)}></div>
+            <div className="screen" onClick={() => setModal(false)}></div>
             <section>
                 <h3>Thank you! You will see it soon on the screen!</h3>
-                <button onClick={setModal(false)}>OK</button>
+                <button onClick={() => setModal(false)}>OK</button>
             </section>
         </>
     )
